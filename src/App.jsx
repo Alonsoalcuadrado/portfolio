9dcf3c50ef26a2ab460283ad3624e432b9a6e441
Erick Alonso <erick.alonso@epicor.com>
2023-02-07 02:01:12 -0600

    dtyles modificated and decorator and photo added

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,11 @@ function App() {
   return (
     <div className="app">
       <Navbar />
-      <h2 className="app-header">Nice to meet you! I'm Adam Keyes.</h2>
-      <p>Based in the UK, I’m a front-end developer passionate about building accessible web apps that users love.</p>
+      <img className='app-header-decoration' src="src/assets/decoration.png" alt="page decoration" />
+      <img className='app-header-photo' src="src/assets/erickalonso.png" alt="profile photo Erick Alonso" />
+      <img className='app-header-oval' src="src/assets/oval.svg" alt="oval decorator" />
+      <h2 className="app-header">Nice to meet you! I'm <span className='app-header-text-underline'>Adam Keyes.</span></h2>
+      <p className='app-header-description'>Based in the UK, I’m a front-end developer passionate about building accessible web apps that users love.</p>
     </div>
   )
 }
