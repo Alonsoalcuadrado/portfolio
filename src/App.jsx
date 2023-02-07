924c05ca460e42d72ae6a8f05f408d4567afb1bd
Erick Alonso <erick.alonso@epicor.com>
2023-02-07 01:11:54 -0600

    styles modificated, navbar created and added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+import Navbar from './components/Navbar'
+
 function App() {
 
   return (
     <div className="app">
+      <Navbar />
       <h2 className="app-header">Nice to meet you! I'm Adam Keyes.</h2>
       <p>Based in the UK, I’m a front-end developer passionate about building accessible web apps that users love.</p>
     </div>
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+const Navbar = () => {
+
+  return (
+    <div className="navbar">
+        <h4 className="navbar-name">
+            adamkeyes
+        </h4>
+        <ul className="navbar-social-logos">
+            <li>logo</li>
+            <li>logo</li>
+            <li>logo</li>
+            <li>logo</li>
+        </ul>
+    </div>
+  )
+}
+
+export default Navbar
